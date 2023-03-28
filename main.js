beccccfb72b9b89a65b599bdedd07350f6d2928a
Jai <jaijai.stell@gmail.com>
2023-03-28 15:56:02 -0700

    updates lab not yet complete

--- main.js
+++ main.js
@@ -5,11 +5,44 @@ const FULL_HEART = "♥";
 //---------------define liking the heart variable make it select heart icons when called later
 console.log("Is my Js loaded?");
 const heartLike = document.querySelectorAll(".like-glyph");
-//hide the modal! error is now the variable for finding the modal through DOM then hiding it
-let error = document.querySelector("#modal");
-error.className = "hidden";
-// Your JavaScript code goes here!
-
+//*!hide the modal! error is now the variable for finding the modal through DOM then hiding it
+const error = document.querySelector("#modal");
+error.classList.add("hidden");
+//classlist. add returns tokens from CSS DOM classnames of element
+//
+//
+//
+//
+//** Jais JavaScript code goes here!
+//
+//
+//
+//
+heartLike.forEach((heart) => {
+  heart.addEventListener("click", () => {
+    likeCallback(heart);
+  });
+});
+//*! forEach = Calls a function for each element, heart is my param, its then called as a function? and on "clicks" the function will be called likeCallBack will be called next
+function likeCallback(heart) {
+  mimicServerCall("dummyUrl")
+    .then((response) => {
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.classList.add("activated-heart");
+      } else if (heart.innerText === FULL_HEART) {
+        heart.innerText = EMPTY_HEART;
+        heart.classList.remove("activated-heart");
+      }
+    })
+    .catch((errors) => {
+      error.classList.remove("hidden");
+      error.querySelector("#modal-message").innerText = errors;
+      setTimeout(() => {
+        error.classList.add("hidden");
+      }, 3000);
+    });
+}
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
